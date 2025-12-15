a10fa5d0c050b9cd32e4b1506c0d1ced42dbe010
loife <andrejdobric7@gmail.com>
2025-12-15 12:30:44 +0100

    Fix routing

--- webapp/src/app/app.routes.ts
+++ webapp/src/app/app.routes.ts
@@ -1,7 +1,4 @@
 import { Routes } from '@angular/router';
-import { ProfileComponent } from './pages/profile';
 
 export const routes: Routes = [
-  { path: '', component: ProfileComponent },
-  { path: 'profile', component: ProfileComponent },
 ];

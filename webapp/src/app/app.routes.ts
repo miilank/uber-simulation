188aa4c8ed4bce002212c4993cf3f8547f45fa42
loife <andrejdobric7@gmail.com>
2025-12-16 13:49:43 +0100

    Change file organisation

--- webapp/src/app/app.routes.ts
+++ webapp/src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { ProfileComponent } from './pages/profile';
+import { ProfileComponent } from './features/registered/pages/profile';
 
 export const routes: Routes = [
   { path: '', component: ProfileComponent },
